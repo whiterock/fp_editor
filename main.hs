3e3ecd1aa2d4e42335ade71f0091a2611aa02340
Fabian Regen <e11701821@student.tuwien.ac.at>
2020-09-24 10:51:23 +0200

    added vertical scrolling

--- main.hs
+++ main.hs
@@ -26,23 +26,17 @@ color (v) = "\ESC[" ++ show (31 + (v `mod` 6)) ++ "m"
 -- remaining string, position, position_to_watch, current_level, level_to_highlight
 type Stream = ([Char], Int, Maybe Int, Maybe Int, Int)
 
-highlight :: Stream -> IO()
+highlight :: Stream -> String
 highlight (s, pos, pos_match_left, pos_match_right, lvl) -- pos_watch is unused as of now, but could be used for putting cursor here.
-  | null s = putStr ""
+  | null s = ""
   | otherwise = do
     let next_level | head s == '(' || head s == '{' = lvl + 1
                    | head s == ')' || head s == '}' = lvl - 1
                    | otherwise = lvl
-    if next_level /= lvl then
-      if next_level > lvl then 
-        --  putStr (color lvl ++ "\ESC[4m" ++ [head s] ++ "\ESC[0m") -- underline
-        putStr (color lvl ++ (if isJust pos_match_left && pos == fromJust pos_match_left then "\ESC[4m" else "") ++ [head s] ++ "\ESC[0m")
-      else
-        --  putStr (color next_level ++ "\ESC[4m" ++ [head s] ++ "\ESC[0m" ) -- underline
-        putStr (color next_level ++ (if isJust pos_match_right && pos == fromJust pos_match_right then "\ESC[4m" else "") ++ [head s] ++ "\ESC[0m" )
-    else
-      putChar (head s)
-    highlight (tail s, pos+1, pos_match_left, pos_match_right, next_level)
+        ret | next_level == lvl = [head s]
+            | next_level > lvl = (color lvl ++ (if isJust pos_match_left && pos == fromJust pos_match_left then "\ESC[4m" else "") ++ [head s] ++ "\ESC[0m")
+            | otherwise = (color next_level ++ (if isJust pos_match_right && pos == fromJust pos_match_right then "\ESC[4m" else "") ++ [head s] ++ "\ESC[0m" )
+    ret ++ highlight (tail s, pos+1, pos_match_left, pos_match_right, next_level)
 
 
 findMatchRight :: ([Char], Int, Int, Int, Maybe Int) -> Maybe Int
@@ -85,54 +79,57 @@ findMatchLeft (s, pos, pos_watch, lvl, lvl_end) = do
   else
     Nothing
 
-type Text = ([Char], [Char])
+type Text = (Int, [Char], [Char])
+
+current_line :: String -> Int
+current_line p = count '\n' p + 1 
 
 editor :: Text -> IO Text
-editor (p,q) = do
-  clear
-  moveCursorTo 0 0
-  if head q == '(' || head q == '{' || head q == ')' || head q == '}' then
-    if head q == '(' || head q == '{' then
-      highlight ((p ++ q), 0, Just (length p), findMatchRight (p++q, 0, length p, 0, Nothing), 0)
-    else
-      -- fixme
-      --highlight ((p ++ q), 0, maybe Nothing (\x -> (length (p++q))-x) (findMatchRight (reverse p++q, 0, length q, 0, Nothing)), Just (length p), 0)
-      highlight ((p ++ q), 0, findMatchLeft (p++q, length (p++q) - 1, length p, 0, Nothing), Just (length p), 0)
-  else
-    highlight ((p ++ q), 0, Nothing, Nothing, 0)
-  -- debugging begin --
-  moveCursorTo 3 0
-  putStr (show (fromMaybe 9 (findMatchRight (p++q, 0, length p, 0, Nothing))))
-  putStr " "
-  putStr (show (fromMaybe 9 (findMatchLeft (p++q, length (p++q) - 1, length p, 0, Nothing))))
-  -- debugging end --
-  --moveCursorTo 0 0
-  --highlight ((p ++ q), 0, head parens, tail parens,  0)
-  moveCursorTo (count '\n' p + 1) (if length p == 0 || last p == '\n' then 1 else (length $ last $ lines p)+1)
-  hFlush stdout
-  c <- getKey
-  if c == "\ESC" then return (p,q) -- promote Text to IO Text
-    else do
-      case c of --TODO: stay at level of indentation? 
-        "\ESC[A" -> if lines p /= [] then editor (unlines $ init $ lines p, (last $ lines p) ++ (if last p == '\n' then "\n" else "") ++ q) else editor (p, q)-- Up
-        "\ESC[B" -> if (length $ lines q) > 1 then editor (p ++ (head $ lines q) ++ "\n", unlines $ tail $ lines q) else editor (p, q)-- Down
-        "\ESC[C" -> if length q > 1 then editor (p ++ [head q], tail q) else editor (p, q) -- Right, needs > 1 for whatever reason
-        "\ESC[D" -> if length p > 0 then editor (init p, last p : q) else editor (p, q) -- Left
-        "\DEL"   -> if not (null p) then -- Delete
-                      if not (null q) && 
-                        ((last p == '(' && head q == ')') || 
-                        (last p == '<' && head q == '>') || 
-                        (last p == '{' && head q == '}')) then
-                        editor (init p, tail q) -- Delete additional parens
-                      else
-                        editor (init p, q)
-                    else 
-                      editor (p, q)             
-        "("      -> editor (p ++ "(", ")" ++ q)
-        "<"      -> editor (p ++ "<", ">" ++ q)
-        "{"      -> editor (p ++ "{", "}" ++ q)
-        "\t"     -> editor (p ++ "  ", q)
-        _        -> editor (p ++ c,q) --TODO: careful about other escape sequences (like up and down arrow.)
+editor (skip,p,q) 
+  | current_line p - skip == 0 = editor (skip-1, p, q)
+  | current_line p - skip > 20 = editor (skip+1, p,q)
+  | otherwise = do
+    clear
+    moveCursorTo 0 0
+    let s | head q == '(' || head q == '{' = highlight ((p ++ q), 0, Just (length p), findMatchRight (p++q, 0, length p, 0, Nothing), 0)
+          | head q == ')' || head q == '}' = highlight ((p ++ q), 0, findMatchLeft (p++q, length (p++q) - 1, length p, 0, Nothing), Just (length p), 0) --fixme see below (fixed?)
+          | otherwise = highlight ((p ++ q), 0, Nothing, Nothing, 0)
+
+    -- TODO: 20 lines max for now?
+    putStr $ unlines $ take 20 $ drop skip $ lines s
+    -- debugging begin --
+    moveCursorTo 3 0
+    putStr (show (fromMaybe 9 (findMatchRight (p++q, 0, length p, 0, Nothing))))
+    putStr " "
+    putStr (show (fromMaybe 9 (findMatchLeft (p++q, length (p++q) - 1, length p, 0, Nothing))))
+    -- debugging end --
+    --moveCursorTo 0 0
+    --highlight ((p ++ q), 0, head parens, tail parens,  0)
+    moveCursorTo (current_line p - skip) (if length p == 0 || last p == '\n' then 1 else (length $ last $ lines p)+1)
+    hFlush stdout
+    c <- getKey
+    if c == "\ESC" then return (skip, p,q) -- promote Text to IO Text
+      else do
+        case c of --TODO: stay at level of indentation? 
+          "\ESC[A" -> if lines p /= [] then editor (skip, unlines $ init $ lines p, (last $ lines p) ++ (if last p == '\n' then "\n" else "") ++ q) else editor (skip, p, q)-- Up
+          "\ESC[B" -> if (length $ lines q) > 1 then editor (skip, p ++ (head $ lines q) ++ "\n", unlines $ tail $ lines q) else editor (skip, p, q)-- Down
+          "\ESC[C" -> if length q > 1 then editor (skip, p ++ [head q], tail q) else editor (skip, p, q) -- Right, needs > 1 for whatever reason
+          "\ESC[D" -> if length p > 0 then editor (skip, init p, last p : q) else editor (skip, p, q) -- Left
+          "\DEL"   -> if not (null p) then -- Delete
+                        if not (null q) && 
+                          ((last p == '(' && head q == ')') || 
+                          (last p == '<' && head q == '>') || 
+                          (last p == '{' && head q == '}')) then
+                          editor (skip, init p, tail q) -- Delete additional parens
+                        else
+                          editor (skip, init p, q)
+                      else 
+                        editor (skip, p, q)             
+          "("      -> editor (skip, p ++ "(", ")" ++ q)
+          "<"      -> editor (skip, p ++ "<", ">" ++ q)
+          "{"      -> editor (skip, p ++ "{", "}" ++ q)
+          "\t"     -> editor (skip, p ++ "  ", q)
+          _        -> editor (skip, p ++ c,q) --TODO: also scroll on \n if in >>last row<< :)
 
 getContent :: String -> IO String
 getContent name = do
@@ -145,13 +142,14 @@ getContent name = do
 
 loop :: String -> Text -> IO ()
 loop n t = do
-        (p, q) <- editor t
+        (s, p, q) <- editor t
         writeFile n (p++q++"\n")
+        clear
         moveCursorTo 0 0
         putStr "      ___           ___                         ___                   \n     /\\__\\         /\\  \\          ___          /\\__\\         _____    \n    /:/ _/_       /::\\  \\        /\\  \\        /:/ _/_       /::\\  \\   \n   /:/ /\\  \\     /:/\\:\\  \\       \\:\\  \\      /:/ /\\__\\     /:/\\:\\  \\  \n  /:/ /::\\  \\   /:/ /::\\  \\       \\:\\  \\    /:/ /:/ _/_   /:/  \\:\\__\\ \n /:/_/:/\\:\\__\\ /:/_/:/\\:\\__\\  ___  \\:\\__\\  /:/_/:/ /\\__\\ /:/__/ \\:|__|\n \\:\\/:/ /:/  / \\:\\/:/  \\/__/ /\\  \\ |:|  |  \\:\\/:/ /:/  / \\:\\  \\ /:/  /\n  \\::/ /:/  /   \\::/__/      \\:\\  \\|:|  |   \\::/_/:/  /   \\:\\  /:/  / \n   \\/_/:/  /     \\:\\  \\       \\:\\__|:|__|    \\:\\/:/  /     \\:\\/:/  /  \n     /:/  /       \\:\\__\\       \\::::/__/      \\::/  /       \\::/  /   \n     \\/__/         \\/__/        ~~~~           \\/__/         \\/__/    \n\n\n\t\t\t Press Any Key to Continue"
         hFlush stdout
         a <- getKey
-        loop n (p,q)
+        loop n (s,p,q)
 
 main :: IO ()
 main = do
@@ -162,4 +160,4 @@ main = do
   hSetBuffering stdin NoBuffering
   hSetEcho stdin False
   clear
-  loop (head args) ([],c)
+  loop (head args) (0,[],c)
